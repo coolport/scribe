58d913cb06d5443585b0facf61adf8daa6ce15f6
coolport <aidanalcayde@gmail.com>
2025-08-24 23:52:26 +0800

    layout mockup pt1
    layoutings

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,6 +1,49 @@
+import { useState, type ChangeEvent, type FormEvent } from 'react';
+import styles from './styles/Index.module.css'
+
+//TODO: 
+//Today: YIPA, no validation, etc. yet, just get it to show
+//Tom: Validation, etc.
+
 function Index() {
+  const [url, setUrl] = useState<string | null>(null);
+
+  const handleSubmit = (): void => {
+    console.log("URL VALUE b4 clear: ", url);
+    setUrl('');
+    console.log("Submitted Form");
+  }
+
   return (
     <>
+      <div className={styles.container}>
+        <div className={styles.left}>
+
+          <p>Enter YouTube Link</p>
+          <form
+            onSubmit={(e: FormEvent<HTMLFormElement>) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <input
+              value={url ?? ''}
+              onChange={(e: ChangeEvent<HTMLInputElement>): void => {
+                const inputValue = e.target.value;
+                setUrl(inputValue);
+                console.log(url);
+              }}
+            />
+            <button
+              type='submit'
+            >Enter</button>
+          </form>
+          <p>for now</p> <br />
+        </div>
+        <div className={styles.right}>
+          right side
+        </div>
+      </div>
     </>
   )
 }
--- src/review/Todo.tsx
+++ src/review/Todo.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState, type ChangeEvent } from "react";
 function Todo() {
   const [userInput, setUserInput] = useState<string>('');
   const [list, setList] = useState<string[]>([]);
-  const [show, setShow] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(true);
 
   const [selectedCheckbox, setSelectedCheckbox] = useState<string[]>([]);
   const [isChecked, setIsChecked] = useState<boolean>(false);
@@ -13,33 +13,37 @@ function Todo() {
     console.log("Updated selectedCheckbox:", selectedCheckbox);
   }, [selectedCheckbox]);
 
-
   // const listItems = list.map( listItem => <li>{listItem}</li>);
   const listItems = list.map((listItem, index) => {
-    return <>
-      <div
-        key={index} //key uppermost
-      >
-        <input
-          type="checkbox"
-          value={listItem}
-          id="inputDiv"
-          checked={selectedCheckbox.includes(listItem)} //if checked, its in the checklist, if thats true, return true for checked
-          onChange={(e) => {
-            const tickValue = e.target.checked;
+    const entryId = `checkbox-${listItem}`;
+    return <div
+      //KEY THEORY: if using a fragment, put it there, basta TOP TOP level talaga. use React.fragment for props/attribs? u cant
+      //index bad practice, use an id or wht. in this case lit using the listItem name is better than using the index
+      key={listItem}
+    >
+      <input
+        type="checkbox"
+        value={listItem}
+        // all entries have the same id, lots of problems. put this elsewhere..
+        // need dynamic, see const entryId above
+        // id="inputDiv"
+        id="entryId"
+        checked={selectedCheckbox.includes(listItem)} //if checked, its in the checklist, if thats true, return true for checked
+        onChange={(e) => {
+          const tickValue = e.target.checked;
+
+          if (!selectedCheckbox.includes(listItem)) {
             setSelectedCheckbox(prev => [...prev, listItem])
-            if (selectedCheckbox.includes(listItem))
-              setSelectedCheckbox(prev => [...prev, listItem])
-            // setIsChecked((tickValue) => {
-            //   tickValue ? setIsChecked(false) : setIsChecked(true)
-            // })
-            setIsChecked((tickValue) => !tickValue) //pwede pala to if functional, pero setIsChecked(!tickValue) nope
-            console.log("e value: ", tickValue);
-          }}
-        />
-        <label htmlFor="inputDiv">{listItem}</label>
-      </div>
-    </>
+          } else {
+            setSelectedCheckbox(prev => prev.filter(item => item !== listItem)); //run through all items, add to to-be returned filter array if cond is true
+          }
+
+
+          console.log("e value: ", tickValue);
+        }}
+      />
+      <label htmlFor="inputDiv">{listItem}</label>
+    </div>
   });
 
   const handleSubmit = () => {

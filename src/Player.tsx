34a9b3ecede63e880dcf230128b0a04fe505f5f9
coolport <aidanalcayde@gmail.com>
2025-08-31 10:27:17 +0800

    feat: Player loading state, better prop handling

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,5 +1,6 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import YouTube, { type YouTubeProps } from "react-youtube";
+import styles from './styles/Player.module.css'
 
 // Import destructured type YouTubeProps from the pkg. AVOID redefining - it's the point of the pkg.
 // we still construct a type PlayerProps with YouTubeProps[] bc 
@@ -11,6 +12,15 @@ type PlayerProps = {
 
 function Player({ videoId, onReady }: PlayerProps) {
   const playerRef = useRef<any>(null); // For youtube player instance
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (videoId) {
+      setIsLoading(true);
+      setIsReady(false);
+    }
+  }, [videoId])
 
   const opts: YouTubeProps['opts'] = {
     height: '390',
@@ -23,6 +33,14 @@ function Player({ videoId, onReady }: PlayerProps) {
   const onPlayerReady: PlayerProps['onReady'] = (event): void => {
     // Assignemnt here because assign to ref only when it's mounted
     playerRef.current = event.target;
+
+    setIsLoading(false);
+    setIsReady(true);
+
+    // For the parent component to pass its own func through the onReady prop
+    if (onReady) {
+      onReady(event);
+    }
 
     // access to player in all event handlers via event.target under onReady
 
@@ -38,14 +56,19 @@ function Player({ videoId, onReady }: PlayerProps) {
   }
 
   return <>
-    {videoId ? <YouTube
-      videoId={videoId}
-      opts={opts}
-      onReady={onReady ?? onPlayerReady} /> : <p>URL is NULL</p>
+    {videoId ?
+      <>
+        {isLoading && <div className={styles.loader}></div>}
+        <YouTube
+          videoId={videoId}
+          opts={opts}
+          onReady={onPlayerReady} />
+      </>
+      : <p>Enter a link to begin taking notes.</p>
     }
 
     {/* TODO:  load components only when player has mounted */}
-    <button onClick={getTimeStamp}>GetTimestamp</button>
+    {isReady && <button onClick={getTimeStamp}>GetTimestamp</button>}
   </>
 }
 

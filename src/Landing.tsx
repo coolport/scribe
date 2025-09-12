91e64b396194803d0401f901a737816c7092bd30
coolport <aidanalcayde@gmail.com>
2025-09-12 23:15:49 +0800

    styling

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -2,6 +2,7 @@ import { useState, type ChangeEvent, type KeyboardEvent, useEffect } from 'react
 import styles from './styles/Home.module.css'
 import extractYouTubeDetails from './utils/extract-id';
 import { useNavigate } from 'react-router';
+import { Input } from '@/components/ui/input';
 
 function Landing() {
   const [url, setUrl] = useState<string | null>(null);
@@ -37,9 +38,10 @@ function Landing() {
     <>
       <div className={styles.left}>
 
-        <p>Enter YouTube Link</p>
+        <p className='align-middle'>Enter YouTube Link</p>
         <form >
-          <input
+          <Input
+            className="rounded-full"
             value={formValue ?? ''}
             onChange={(e: ChangeEvent<HTMLInputElement>): void => {
               const inputValue = e.target.value;

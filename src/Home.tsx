c21c621532199d87aff7352180b2d965f8ede37c
coolport <aidanalcayde@gmail.com>
2025-09-29 10:09:30 +0800

    feat(styling): tw layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,4 @@
 import { useRef } from 'react';
-import styles from './styles/Home.module.css'
 import Player from './Player';
 import ChatBox from './ChatBox';
 import { useParams } from 'react-router';
@@ -7,24 +6,19 @@ import { useParams } from 'react-router';
 function Home() {
   const playerRef = useRef<any>(null);
   const params = useParams();
-
-  // const vidUrl = location.state.url;
   const vidUrl = params.videoUrl || '';
+
   return (
-    <>
-      <div className={styles.mainContainer}>
-        <div className={styles.left}>
-          <div id="playerContainer">
-            <Player videoId={vidUrl} ref={playerRef}></Player>
-          </div>
-        </div>
-        <div className={styles.right}>
-          <div>
-            <ChatBox playerRef={playerRef} />
-          </div>
+    <div className="flex w-full h-screen bg-gray-100">
+      <div className="w-[70%] h-full">
+        <div id="playerContainer" className="w-full h-full">
+          <Player videoId={vidUrl} ref={playerRef}></Player>
         </div>
       </div>
-    </>
+      <div className="w-[30%] h-full p-4 bg-white border-l border-gray-200">
+        <ChatBox playerRef={playerRef} />
+      </div>
+    </div>
   )
 }
 

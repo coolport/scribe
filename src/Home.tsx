3975a577cc1cc51b82e34a126d17d0f4ddb36a2e
coolport <aidanalcayde@gmail.com>
2025-12-20 16:22:03 +0800

    fill vertically

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,15 +11,15 @@ function Home() {
 
   return (
     <div className="flex flex-col md:flex-row w-full h-screen bg-background overflow-hidden">
-      <div className="w-full md:w-[70%] flex flex-col items-center justify-start bg-black">
-        <div className="relative w-full" style={{ aspectRatio: '16 / 9' }}>
+      <div className="w-full md:w-[70%] flex flex-col items-stretch justify-start bg-black">
+        <div className="relative w-full flex-shrink-0" style={{ aspectRatio: '16 / 9' }}>
           <Player videoId={vidUrl} ref={playerRef} />
         </div>
-        <div className="flex justify-center space-x-4 p-4">
-          <Button>Playback -5s</Button>
-          <Button>Play/Pause</Button>
-          <Button>Playback +5s</Button>
-          <Button>Loop</Button>
+        <div className="grid grid-cols-4 gap-4 p-4 flex-grow">
+          <Button className="h-full">Playback -5s</Button>
+          <Button className="h-full">Play/Pause</Button>
+          <Button className="h-full">Playback +5s</Button>
+          <Button className="h-full">Loop</Button>
         </div>
       </div>
       <div className="w-full md:w-[30%] p-4 bg-card border-t md:border-t-0 md:border-l border-border flex flex-col flex-grow">

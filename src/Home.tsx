933d8d4997b2aac2feae10c0eae85929bca51edc
coolport <aidanalcayde@gmail.com>
2025-12-20 16:14:01 +0800

    feat: buttons start

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import Player from './Player';
 import ChatBox from './ChatBox';
 import { useParams } from 'react-router';
+import { Button } from '@/components/ui/button';
 
 function Home() {
   const playerRef = useRef<any>(null);
@@ -10,9 +11,15 @@ function Home() {
 
   return (
     <div className="flex flex-col md:flex-row w-full h-screen bg-background overflow-hidden">
-      <div className="w-full md:w-[70%] flex items-start justify-center bg-black">
+      <div className="w-full md:w-[70%] flex flex-col items-center justify-start bg-black">
         <div className="relative w-full" style={{ aspectRatio: '16 / 9' }}>
           <Player videoId={vidUrl} ref={playerRef} />
+        </div>
+        <div className="flex justify-center space-x-4 p-4">
+          <Button>Playback -5s</Button>
+          <Button>Play/Pause</Button>
+          <Button>Playback +5s</Button>
+          <Button>Loop</Button>
         </div>
       </div>
       <div className="w-full md:w-[30%] p-4 bg-card border-t md:border-t-0 md:border-l border-border flex flex-col flex-grow">

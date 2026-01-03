8b1fd59a9b3a5671b930407e0406196a2cbbeef9
coolport <aidanalcayde@gmail.com>
2026-01-03 14:07:25 +0800

    feat: dev server and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 import Home from './Home.tsx'
 import Landing from './Landing.tsx'
-import Test from './Test.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import OAuth2RedirectHandler from './OAuth2RedirectHandler.tsx'
 import './index.css'
+
+console.log('main.tsx loading...');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/video/:videoUrl" element={<Home />} />
-        <Route path="test" element={<Test />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/video/:videoUrl" element={<Home />} />
+          <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+          <Route path="*" element={
+            <div style={{ padding: '2rem', textAlign: 'center' }}>
+              <h1>404 - Not Found</h1>
+              <p>The page you are looking for does not exist.</p>
+              <p>Current path: {window.location.pathname}</p>
+            </div>
+          } />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 )
+
+console.log('main.tsx loaded');
